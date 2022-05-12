593dc50be0d3cf1d1dd8279dcf6720202521c6e1
jhongo <jhon_gom22@outlook.com>
2022-05-11 22:38:01 -0500

    Interfaz SelectedAvatar

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MaterialApp(
       title: 'Material App',
-      initialRoute: 'register',
+      initialRoute: 'select',
       debugShowCheckedModeBanner: false,
       routes: {
         'register': (context) => const  RegisterScreen(),
--- lib/screens/select_avatar_screen.dart
+++ lib/screens/select_avatar_screen.dart
@@ -6,10 +6,61 @@ class SelectAvatarScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('SelectAvatarScreen'),
+    return Scaffold(
+      body: _BackgroundMain(
+        child: Column(
+          children:const [
+            SizedBox(height: 70,),
+            _TitleAvatar(),
+            SizedBox(height: 120,),
+            Text('Descubre tu personaje!', style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w500),)
+          
+            
+          ]),
+      )
+    );
+  }
+}
+
+class _TitleAvatar extends StatelessWidget {
+  const _TitleAvatar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children:const [
+          Text('Bienvenido', style: TextStyle(color: Colors.white, fontSize: 20,),),
+          Text('Kevin', style: TextStyle(color: Colors.white, fontSize: 50, fontWeight: FontWeight.bold),),
+          
+
+        ],
       ),
+    );
+  }
+}
+
+class _BackgroundMain extends StatelessWidget {
+
+  final Widget child;
+
+  const _BackgroundMain({
+    Key? key,required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      width: size.width,
+      height: size.height,
+      color: Color(0xffEA5055),
+      child: child,
     );
   }
 }
